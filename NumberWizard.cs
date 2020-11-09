b571310f89477a790df20b07f886f3696d4f78e4
JP3tri3 <57971582+JP3tri3@users.noreply.github.com>
2020-11-09 16:31:50 -0500

    Add files via upload
    Added text output & calculate guess.

--- NumberWizard.cs
+++ NumberWizard.cs
@@ -10,22 +10,28 @@ public class NumberWizard : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
 
-        Debug.Log("Welcome to Number Wizard, human");
+        Debug.Log("Welcome human.. to the Number Wizard!");
         Debug.Log("Pick a number, don't tell me...");
         Debug.Log("The highest number to pick is: " + max);
         Debug.Log("The lowest number to pick is: " + min);
         Debug.Log("Tell me if your number is higher or lower than " + guess);
         Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+        max = max + 1;
     }
 
     // Update is called once per frame
     void Update() {
 
         if (Input.GetKeyDown(KeyCode.UpArrow)) {
-            Debug.Log("Up Arrow key was pressed.");
             min = guess;
+            guess = (max + min) / 2;
+            Debug.Log("Is it higher or lower than " + guess + "?");
+            Debug.Log(guess);
         } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
-            Debug.Log("Down Arrow key was pressed.");
+            max = guess;
+            guess = (max + min) / 2;
+            Debug.Log("Is it higher or lower than " + guess + "?");
+            Debug.Log(guess);
         } else if (Input.GetKeyDown(KeyCode.Return)) {
             Debug.Log("Enter key was pressed.");
         }
