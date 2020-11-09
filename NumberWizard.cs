e49001769c140ebdd0f1936a8bd567705fa88440
JP3tri3 <57971582+JP3tri3@users.noreply.github.com>
2020-11-09 17:26:41 -0500

    Add files via upload
    Ready ~ Pre UI

--- NumberWizard.cs
+++ NumberWizard.cs
@@ -9,7 +9,14 @@ public class NumberWizard : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start() {
+        StartGame();
 
+    }
+
+    void StartGame() {
+        max = 1000;
+        min = 1;
+        guess = 500;
         Debug.Log("Welcome human.. to the Number Wizard!");
         Debug.Log("Pick a number, don't tell me...");
         Debug.Log("The highest number to pick is: " + max);
@@ -24,17 +31,21 @@ public class NumberWizard : MonoBehaviour {
 
         if (Input.GetKeyDown(KeyCode.UpArrow)) {
             min = guess;
-            guess = (max + min) / 2;
-            Debug.Log("Is it higher or lower than " + guess + "?");
-            Debug.Log(guess);
+            NextGuess();
         } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
             max = guess;
-            guess = (max + min) / 2;
-            Debug.Log("Is it higher or lower than " + guess + "?");
-            Debug.Log(guess);
+            NextGuess();
         } else if (Input.GetKeyDown(KeyCode.Return)) {
-            Debug.Log("Enter key was pressed.");
+            Debug.Log("This was the correct number!");
+            StartGame();
         }
 
     }
+
+    void NextGuess() {
+        guess = (max + min) / 2;
+        Debug.Log("Is it higher or lower than " + guess + "?");
+    }
+
+
 }
